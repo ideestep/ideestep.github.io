1fc214218be86d13266a1a4cb4c87a8bbc88ea4f
Ideestep <ideestep@gmail.com>
2026-01-30 12:20:25 +0530

    Update includes.js

--- assets/js/includes.js
+++ assets/js/includes.js
@@ -1,1 +1,12 @@
+function loadHTML(id, file) {
+  fetch(file)
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
+    })
+    .catch(err => console.error(`Error loading ${file}`, err));
+}
+
+loadHTML("site-header", "includes/header.html");
+loadHTML("site-footer", "includes/footer.html");
 
